032e8e1c33c95778ba8997b7f26854b004fd8d38
al2407111 <nwxx2407-7@yandex.ru>
2024-07-19 20:36:00 +0300

    4

--- app/src/main/java/com/example/playlistmaker/ActivityMedia.kt
+++ app/src/main/java/com/example/playlistmaker/ActivityMedia.kt
@@ -3,16 +3,16 @@ package com.example.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class ActivityMedia :  AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media)
-        val backToMainMedia = findViewById<Button>(R.id.button_back)
-        backToMainMedia.setOnClickListener {
-            val backToMainIntent = Intent(this, MainActivity::class.java)
-            startActivity(backToMainIntent)
+        val backToMainSettings = findViewById<ImageView>(R.id.button_back)
+        backToMainSettings.setOnClickListener {
+            super.finish()
         }
     }
 }
--- app/src/main/java/com/example/playlistmaker/ActivitySearch.kt
+++ app/src/main/java/com/example/playlistmaker/ActivitySearch.kt
@@ -3,16 +3,16 @@ package com.example.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class ActivitySearch : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-        val backToMainSearch = findViewById<Button>(R.id.button_back)
-        backToMainSearch.setOnClickListener {
-            val backToMainIntent = Intent(this, MainActivity::class.java)
-            startActivity(backToMainIntent)
+        val backToMainSettings = findViewById<ImageView>(R.id.button_back)
+        backToMainSettings.setOnClickListener {
+            super.finish()
         }
     }
 }
