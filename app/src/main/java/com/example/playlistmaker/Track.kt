5e476450d8bd97460539fec03927ebc10ee79487
al2407111 <nwxx2407-7@yandex.ru>
2024-09-20 19:48:01 +0300

    sprint13__20_09

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -3,10 +3,20 @@ package com.example.playlistmaker
 import com.google.gson.annotations.SerializedName
 
 data class Track(
+
   val trackName: String= "",      // Название композиции
   val artistName: String= "",     // Имя исполнителя
   var trackTime: Long=0,      // Продолжительность трека в формате mm:ss
   val trackTimeMillis: Int,  // Продолжительность трека в миллисекундах
   val artworkUrl100: String= "" ,  // Ссылка на изображение обложки
-  val trackId: Int=0
+
+  val trackId: Int = 0,
+  val collectionName : String,
+  val releaseDate: String ,
+  val primaryGenreName: String ,
+  val country: String
 )
+{
+  fun getCoverArtwork() = artworkUrl100.replaceAfterLast('/',"512x512bb.jpg")
+  fun getYear() = releaseDate.substringBefore('-')
+}
